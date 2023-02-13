4b8f67f71018b03427a180e405b3951a38bb65f2
mariannapsyllou <117476668+mariannapsyllou@users.noreply.github.com>
2023-02-13 18:34:18 +0100

    Visual numbers

--- Classes/dice_visual.py
+++ Classes/dice_visual.py
@@ -1,46 +1,47 @@
 def dice1():
-    print(" ---------- ")
-    print("|          |")
-    print("|    xx    |")
-    print("|          |")
-    print(" ----------")
+    print(" -----------")
+    print("|    |     |")
+    print("|    |     |")
+    print("|    |     |")
+    print(" -----------")
+
 
 
 def dice2():
-    print(" ---------- ")
-    print("|          |")
-    print("|  xx  xx  |")
-    print("|          |")
+    print(" ----------")
+    print("|   ____  |")
+    print("|   ____| |")
+    print("|  |_____ |")
     print(" ----------")
 
 
 def dice3():
-    print(" ---------- ")
-    print("| xx       |")
-    print("|    xx    |")
-    print("|       xx |")
+    print(" ----------")
+    print("|  ____   |")
+    print("|  ____|  |")
+    print("|  ____|  |")
     print(" ----------")
 
 
 def dice4():
-    print(" ---------- ")
-    print("| xx    xx |")
-    print("|          |")
-    print("| xx    xx |")
+    print(" ----------")
+    print("| |    |  |")
+    print("| |____|  |")
+    print("|      |  |")
     print(" ----------")
 
 
 def dice5():
-    print(" ---------- ")
-    print("| xx    xx |")
-    print("|    xx    |")
-    print("| xx    xx |")
+    print(" ----------")
+    print("|  ___    |")
+    print("| |___    |")
+    print("|  ___|   |")
     print(" ----------")
 
 
 def dice6():
-    print(" ---------- ")
-    print("| xx    xx |")
-    print("| xx    xx |")
-    print("| xx    xx |")
-    print(" ----------")
+    print(" -----------")
+    print("|  ___     |")
+    print("| |___     |")
+    print("| |___|    |")
+    print(" -----------")
