bff051b4532f0999eadcc404aee5a181a8b6832e
mariannapsyllou <117476668+mariannapsyllou@users.noreply.github.com>
2023-02-14 20:52:22 +0100

    added update function in highscores

--- Classes/Highscores.py
+++ Classes/Highscores.py
@@ -14,12 +14,19 @@ class Highscores():
         if player in score_dict:
             print(f"{player} you have won the game {score_dict.get(player)} times!!")
         else:
-            print(f"You have not any registered scores yet")
+            print("You have not any registered scores yet")
 
     def view_instructions(self):
         with open("Instructions.txt", "r") as filename:
             for line in filename:
                 print(line)
 
-
-   
+    def update(self, player, new_win):
+        update_dict = self.dict_results()
+        if player in update_dict:
+            update_dict[player] += new_win
+        else:
+            update_dict[player] = new_win
+        with open("Results.txt", "w") as filename:
+            for key, value in update_dict.items():
+                filename.write(f"{key},{value}")
