bdfac548813991c5e040e368fe0a1d3edc822eda
axelfriberg1 <axelfriberg01@gmail.com>
2023-02-22 22:31:42 +0100

    Testing for singel_player menu

---
+++ src/test_dice/test_menu.py
@@ -0,0 +1,29 @@
+import unittest
+from unittest.mock import patch
+from src.dice.menu import *
+
+class TestMenus(unittest.TestCase):
+
+
+    def test_main_menu(self):
+        output = ""
+        output += "Welcome! Enter help to see commands\n"
+        output += "1- Two players\n2- Single-Player\n"
+        output += "3- Show highscore\n4- Exit"
+        self.assertEqual(main_menu(), output)
+
+    @patch('builtins.input', side_effect=['name1', 'name2'])
+    def test_two_player_names(self, mock_input):
+        player1, player2 = two_player_menu()
+        self.assertEqual(player1, 'name1')
+        self.assertEqual(player2, 'name2')
+
+    @patch('builtins.input', side_effect=['player1', '1'])
+    @patch('src.dice.menu.set_difficulty', return_value='easy')
+    def test_single_player_name_and_difficulty(self, mock_set_difficulty, mock_input):
+        player1, difficulty = single_player_menu()
+        self.assertEqual(player1, 'player1')
+        self.assertEqual(difficulty, 'easy')
+
+if __name__ == "__main__":
+    unittest.main()
