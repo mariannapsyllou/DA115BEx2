9bb76266d5f0c51cd39ec0f90fd676f0dbbcf3c9
mariannapsyllou <117476668+mariannapsyllou@users.noreply.github.com>
2023-02-23 12:36:23 +0100

    small fix in test_highscores

--- src/test_dice/test_highscores.py
+++ src/test_dice/test_highscores.py
@@ -4,20 +4,20 @@ from src.dice.Highscores import Highscores
 
 
 class TestHighscores(unittest.TestCase):
+    def setUp(self) -> None:
+        self.highscores = Highscores()
 
     def test_dict_results(self):
-        high = Highscores()
         mock_file = "Alex,10\nKalle,2\n"
         with patch("builtins.open", mock_open(read_data=mock_file)) \
                 as mock_file:
-            result_dict = high.dict_results()
+            result_dict = self.highscores.dict_results()
         expected_out = {"Alex": 10, "Kalle": 2}
         self.assertEqual(result_dict, expected_out)
 
     def test_dict_fileNotFound(self):
-        high = Highscores()
         with patch("builtins.open", side_effect=FileNotFoundError):
-            result_dict = high.dict_results()
+            result_dict = self.highscores.dict_results()
         expected_out = {}
         self.assertEqual(result_dict, expected_out)
 
